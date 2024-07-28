e1b1dd355840b7e29ec66472395ed4d3908bc36d
bgrando24 <bgrando24@gmail.com>
2024-07-28 10:58:01 +1000

    Implementing logger.cpp

---
+++ src/engines/logging/Logger.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file Logger.cpp
+ * @brief Implementation of the Logger class
+ */
+
+#include "Logger.h"
+
+Logger &Logger::GetInstance()
+{
+    static Logger instance;
+    return instance;
+}
+
+void Logger::Log(const std::string &message, LogLevel level)
+{logMutex
+    if (level < logLevel)
+        return;
+
+    std::lock_guard<std::mutex> lock(logMutex);
+    std::string logMessage = GetLogLevelString(level) + ": " + message;
+
+    if (logFile.is_open())
+    {
+        logFile << logMessage << std::endl;
+    }
+    else
+    {
+        std::cout << logMessage << std::endl;
+    }
+}
+
+void Logger::SetLogFile(const std::string &filename)
+{
+    std::lock_guard<std::mutex> lock(logMutex);
+    if (logFile.is_open())
+    {
+        logFile.close();
+    }
+    logFile.open(filename, std::ios::app);
+}
+
+void Logger::SetLogLevel(LogLevel level)
+{
+    logLevel = level;
+}
+
+Logger::~Logger()
+{
+    if (logFile.is_open())
+    {
+        logFile.close();
+    }
+}
+
+std::string Logger::GetLogLevelString(LogLevel level)
+{
+    switch (level)
+    {
+    case INFO:
+        return "INFO";
+    case WARN:
+        return "WARN";
+    case ERROR:
+        return "ERROR";
+    case DEBUG:
+        return "DEBUG";
+    default:
+        return "UNKNOWN";
+    }
+}
